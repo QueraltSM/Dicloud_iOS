9d4c0c12a299df58bc5b48385e47777d2e1507f6
QueraltSosaMompel <queraltsosa@hotmail.com>
2020-01-18 11:04:49 +0000

    Show alert when login is not successful

--- Dicloud/Login/ViewController.swift
+++ Dicloud/Login/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var nickLbl: UILabel!
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var passwordlbl: UILabel!
-    @IBOutlet weak var errorLabel: SSPaddingLabel!
     
     var textFieldBtn: UIButton {
         let button = UIButton(type: .custom)
@@ -67,8 +66,6 @@ class ViewController: UIViewController {
         nickname.setUnderline(color: UIColor.gray)
         username.setUnderline(color: UIColor.gray)
         password.setUnderline(color: UIColor.gray)
-        errorLabel.layer.cornerRadius = 10
-        errorLabel.layer.masksToBounds = true
         password.rightView = textFieldBtn
         password.rightViewMode = .always
     }
@@ -229,50 +226,5 @@ class ViewController: UIViewController {
                 alert.dismiss(animated: true, completion: nil)
             }
         }
-    }
-}
-
-
-class SSPaddingLabel: UILabel {
-    var padding : UIEdgeInsets
-    
-    // Create a new SSPaddingLabel instance programamtically with the desired insets
-    required init(padding: UIEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)) {
-        self.padding = padding
-        super.init(frame: CGRect.zero)
-    }
-    
-    // Create a new SSPaddingLabel instance programamtically with default insets
-    override init(frame: CGRect) {
-        padding = UIEdgeInsets.zero // set desired insets value according to your needs
-        super.init(frame: frame)
-    }
-    
-    // Create a new SSPaddingLabel instance from Storyboard with default insets
-    required init?(coder aDecoder: NSCoder) {
-        padding = UIEdgeInsets.zero // set desired insets value according to your needs
-        super.init(coder: aDecoder)
-    }
-    
-    override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-        let customRect = rect.inset(by: insets)
-        super.drawText(in: customRect)
-    }
-    
-    // Override `intrinsicContentSize` property for Auto layout code
-    override var intrinsicContentSize: CGSize {
-        let superContentSize = super.intrinsicContentSize
-        let width = superContentSize.width + padding.left + padding.right
-        let heigth = superContentSize.height + padding.top + padding.bottom
-        return CGSize(width: width, height: heigth)
-    }
-    
-    // Override `sizeThatFits(_:)` method for Springs & Struts code
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-        let superSizeThatFits = super.sizeThatFits(size)
-        let width = superSizeThatFits.width + padding.left + padding.right
-        let heigth = superSizeThatFits.height + padding.top + padding.bottom
-        return CGSize(width: width, height: heigth)
     }
 }
